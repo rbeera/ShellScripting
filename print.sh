2184dcff83981a3d5895e3a394c9a6d79566aa04
rbeera <rbeera79@gmail.com>
2021-03-25 15:30:44 +1100

    Added new Shebang file

--- print.sh
+++ print.sh
@@ -7,3 +7,20 @@ echo Welcome to DevOps practice
 echo -e "Hello\nWorld"
 echo -e "Hello\n\nWorld"
 
+#color based printing
+# Colors      Code
+
+# Red         31
+# Green       32
+# Yellow      33
+# Blue        34
+# Magenta     35
+# Cyan        36
+
+# Reset       0
+# Bold        1
+
+# Syntax: echo -e "\e[COLCODEnMESSAGE"
+
+echo -e "\e[31nHello in Red"
+echo -e "\e[1;31nHello in Bold Red"
