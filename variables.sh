13fa9d37b83635443d754e1cf0720dc8761cc848
rbeera <rbeera79@gmail.com>
2021-03-29 09:46:02 +1100

    Variable added

---
+++ variables.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Variable is name you give to certain set of data
+## Without Variables
+
+echo Welcome to DevOps training
+echo Training of DevOps lasts for 70 days
+echo "DevOps training covers shell scripting & Ansible"
+
+## Declaring a variable , Syntax: var = data
+
+COURSE = DevOps
+echo Welcome to ${COURSE} training
+echo Training of $COURSE lasts for 70 days
+echo "$COURSE training covers shell scripting & Ansible"
