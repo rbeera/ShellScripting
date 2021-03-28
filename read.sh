893eb13d8b2cf87770bd5eede3a672fb5fcf12eb
rbeera <rbeera79@gmail.com>
2021-03-29 10:33:09 +1100

    read file added 1

--- read.sh
+++ read.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-read p "Enter your name:" name
-read p "Enter your course:" course
+read p 'Enter your name:' name
+read p 'Enter your course:' course
 
 echo -e "\n Name = $name, Course = $course"
