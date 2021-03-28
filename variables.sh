5a6a73c302774a8b2c354f12158b4af087b0cda8
rbeera <rbeera79@gmail.com>
2021-03-29 09:55:09 +1100

    Variable added 3

--- variables.sh
+++ variables.sh
@@ -13,3 +13,9 @@ COURSE=Linux
 echo Welcome to ${COURSE} training
 echo Training of $COURSE lasts for 70 days
 echo "$COURSE training covers shell scripting & Ansible"
+
+## Variables usually holds a data which varies from time to time
+a=100
+fruit=Apple
+echo $fruit is $ano
+echo $fruit is ${a}ano
