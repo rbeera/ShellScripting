8d1d554d525cf5dff7ba7e00a8f6abc235818a32
rbeera <rbeera79@gmail.com>
2021-03-29 14:44:38 +1100

    functions added

--- Project/Components/frontend.sh
+++ Project/Components/frontend.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-PRINT "Installing Nginx"
-yum install nginx -y
-STAT $? "Nginx Installation"
 
-echo "----------------------------------------------------------------------------------------------"
-exit
+yum install nginxcccc -y
 
-# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-master README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -ne 0 ]; then
+  echo -e "\e[1,31mNginx Installation is failure\e[0m"
+  exit 2
+fi
+
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
