1e404ad5aa3ea6c5238542fc792b3f66d78619e4
rbeera <rbeera79@gmail.com>
2021-03-29 13:44:35 +1100

    exit n common

--- Project/Components/frontend.sh
+++ Project/Components/frontend.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-
-
-
+PRINT "Installing Nginx"
 yum install nginx -y
+STAT $? "Nginx Installation"
+
+echo "----------------------------------------------------------------------------------------------"
+echo -e "\e[1;35m [INFO] Download Frontend Component\e[0m"
+echo "----------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------"
+# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-master README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
---
+++ functions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Determine a function
+
+function abc() {
+  echo "Welcome to abc function"
+}
+
+xyz () {
+  echo "Welcome to xyz function"
+}
+
+## call the function
+abc
+xyz
