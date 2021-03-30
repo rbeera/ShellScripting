21226d9d735b382d93c4fb9343e4d90dc419a792
rbeera <rbeera79@gmail.com>
2021-03-30 14:17:00 +1100

    Case n Create added 3

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -2,4 +2,11 @@
 
 LID=lt-092b7f8b625ae423e
 LVER=4
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} | jq
+COMPONENT=$1
+
+if [ -z "${COMPONENT}"]; THEN
+  echo "Component Name Input is needed"
+  exit 1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" | jq
