ab318cc3786a3df5073817f9f5612e08f3c7d484
rbeera <rbeera79@gmail.com>
2021-03-30 16:40:28 +1100

    Mongodb created and makefile updated 3

--- Project/Components/mongodb.sh
+++ Project/Components/mongodb.sh
@@ -5,12 +5,11 @@ COMPONENT=Mongodb
 
 PRINT "Setup MongoDB Repository"
 echo  '[mongodb-org-4.2]
-  name=MongoDB Repository
-  baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-  gpgcheck=1
-  enabled=1
-  gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 STAT $? "Setup MongoDB repository"
 
 PRINT "Install MongoDB"
