8db440525404de9c82210eea4ecc4f43d5564414
rbeera <rbeera79@gmail.com>
2021-03-30 14:37:35 +1100

    Case n Create added 5

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -9,4 +9,4 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 fi
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
