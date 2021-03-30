11e61ec91244927d18c0e238799fb34c8f72ef11
rbeera <rbeera79@gmail.com>
2021-03-30 11:55:22 +1100

    Source command added

--- Project/Components/frontend.sh
+++ Project/Components/frontend.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
+SOURCE Components/common.sh
+
 PRINT "Installing Nginx"
 yum install nginx -y
 STAT $? "Nginx Installation"
 
 exit
 
+PRINT "Download Frontend Component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+STAT $? "Download Frontend"
+
+PRINT "Extract Frontend"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
+STAT $? "Extracting Frontend"
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
+
+PRINT "Nginx Configuration Update"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $? "Nginx Configuration Updating"
+
+PRINT " Nginx Enabling"
+systemctl enable nginx
+systenctl restart nginx
+STAT $? " Nginx restarting"
--- exit.sh
+++ exit.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 
+# returns exit status for every command
+# status is numbers 0-255
+# 0 success
+# anything other than 0 is non success or partial success
