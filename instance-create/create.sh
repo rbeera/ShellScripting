838e425a89572e66457d93e6a08df27beb04f20d
rbeera <rbeera79@gmail.com>
2021-03-31 16:09:35 +1100

    user n cart, create are updated 8

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -9,9 +9,9 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 fi
 
-INSTANCE_EXISTS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[])
+INSTANCE_EXISTS=$(aws ec2 describe-instances    --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[])
 STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[].Instances[].State.Name | xargs)
-if [ -z "INSTANCE EXISTS" -o "$STATE" == "terminated" ]; then
+if [ -z "${INSTANCE_EXISTS}" -o "$STATE" == "terminated" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 else
   echo "Instance ${COMPONENT} already exists "
@@ -20,4 +20,4 @@ fi
 IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[].Instances[].PrivateIpAddress| grep -v null | xargs)
 
 sed -e "s/COMPONENT/${COMPONENT}/" -e "/s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z091806728R3N2OLW5D8Q --change-batch file:///tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z091806728R3N2OLW5D8Q --change-batch=file:///tmp/record.json
