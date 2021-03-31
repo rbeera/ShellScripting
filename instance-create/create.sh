8c5c532ae62aa41faace7749e81abf71e01eba3d
rbeera <rbeera79@gmail.com>
2021-03-31 14:31:00 +1100

    user n cart, create are updated 7

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -17,7 +17,7 @@ else
   echo "Instance ${COMPONENT} already exists "
 fi
 
-IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[].Instances[].PrivateIpAddress)| grep -v null | xargs)
+IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[].Instances[].PrivateIpAddress| grep -v null | xargs)
 
 sed -e "s/COMPONENT/${COMPONENT}/" -e "/s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z091806728R3N2OLW5D8Q --change-batch file:///tmp/record.json
