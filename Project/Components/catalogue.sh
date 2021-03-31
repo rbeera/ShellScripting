410e15f506feccbe352752222e5c6564161bc318
rbeera <rbeera79@gmail.com>
2021-03-31 12:03:56 +1100

    added catalogue

--- Project/Components/catalogue.sh
+++ Project/Components/catalogue.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
 
-echo Installing catalogue
+PRINT "Install nodejs"
+yum install nodejs make gcc-c++ -y
+STAT $? "Installing nodejs"
+
+PRINT "Create app user"
+useradd roboshop
+STAT $? "Creating app user"
+
+PRINT "Download Catalogue"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+STAT $? "Downloading Catalogue component"
+
+PRINT "Extract Catalogue"
+cd /home/roboshop
+rm -rf catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue
+STAT $? "Extracting Catalogue"
+
+PRINT "Download nodejs dependencies"
+cd /home/roboshop/catalogue
+npm install
+STAT $? "Downloading Dependencies"
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
+
