8f56484e50b9dd8a989a3e078c1973d15e78325d
rbeera <rbeera79@gmail.com>
2021-04-06 21:53:01 +1000

    completed Payment file

--- Project/Components/payment.sh
+++ Project/Components/payment.sh
@@ -4,3 +4,15 @@ source Components/common.sh
 COMPONENT=payment
 
 OS_Prerequisites
+
+PRINT "Install Python 3"
+yum install python36 gcc python3-devel -y
+STAT $? "Installing Python"
+Roboshop_Add_App_User
+Download_Component_From_GitHub
+Extract_Component
+
+PRINT "Install the dependencies"
+cd /home/roboshop/payment
+pip3 install -r requirements.txt
+STAT $? "Installing dependencies"
