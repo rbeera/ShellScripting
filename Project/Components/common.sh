002feba1e82f824df17a699c2a97408037bf5dd8
rbeera <rbeera79@gmail.com>
2021-04-06 12:32:01 +1000

    updated setupservice function with correct syntax for sed command

--- Project/Components/common.sh
+++ Project/Components/common.sh
@@ -75,13 +75,13 @@ Install_NodeJS_Dependencies() {
 Setup_Service(){
   PRINT "Setup SystemD Service for ${COMPONENT}"
   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-  sed -i  -e 's/MONGO_DNSNAME/mongodb.devopspractice.tk/' \
-          -e "s/MONGO_ENDPOINT/mongodb.devopspractice.tk/" \
-          -e "s/REDIS_ENDPOINT/redis.devopspracatice.tk/"  \
-          -e "s/CATALOGUE_ENDPOINT/catalogue.devopspracatice.tk/" \
-          -e "s/DBHOST/mysql.devopspractice.tk/" \
-          -e "/s/CART_ENDPOINT/cart.devopspractice.tk/" \
-          /etc/systemd/system/${COMPONENT}.service
+  sed -i -e 's/MONGO_DNSNAME/mongodb.devopspractice.tk/' \
+         -e "s/MONGO_ENDPOINT/mongodb.devopspractice.tk/" \
+         -e "s/REDIS_ENDPOINT/redis.devopspracatice.tk/"  \
+         -e "s/CATALOGUE_ENDPOINT/catalogue.devopspracatice.tk/" \
+         -e "s/DBHOST/mysql.devopspractice.tk/" \
+         -e 's/CARTENDPOINT/cart.devopspractice.tk/' \
+         /etc/systemd/system/${COMPONENT}.service
   systemctl daemon-reload && systemctl restart ${COMPONENT} && systemctl enable ${COMPONENT}
   STAT $? " Starting ${COMPONENT} Service"
 }
